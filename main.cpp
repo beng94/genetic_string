5752ea89b2563d11c7892903cae9fd101a97cee3
beng94 <gedai.bence@gmail.com>
2015-05-07 11:48:18 +0200

    works fine

--- main.cpp
+++ main.cpp
@@ -4,121 +4,137 @@
 #include <vector>
 #include <iostream>
 #include <stdio.h>
+#include <algorithm>
 
-const int mutate_pro = 15;
-const int crossover_pro = 40;
+const int mutate_pro = 10;
 const int pop_size = 100;
-const std::string goal = "BENCE";
+const int max_gen = 5000;
+const std::string goal = "BENCUSMAKESMAGIC";
 
-int rand_num(int n)
+inline int rand_num(int n)
 {
     return std::rand() % n;
 }
 
 std::string rand_str(int n)
 {
-    srand(time(NULL));
-
     std::string tmp = "";
     for(int i = 0; i<n; i++)
     {
-        tmp += 'A' + rand_num('Z' - 'A');
+        tmp += 'A' + rand_num('Z' - 'A' + 1 );
     }
 
     return tmp;
 }
 
-int fitness (std::string str)
+class Entity
 {
-    int cnt = 0;
-    for(int i = 0; i<str.length(); i++)
-    {
-        if(goal[i] == str[i]) cnt++;
-    }
-
-    return cnt;
-}
-
-void mutate (std::string& str)
-{
-    if(rand_num(100) < mutate_pro)
-    {
-        int id = rand_num(str.length());
-        str[id] = rand_num('Z' - 'A');
-    }
-}
-
-void crossover (std::string& a, std::string& b)
-{
-    if(fitness(a) > fitness(b))
-    {
-        for(int i = 0; i<a.length(); i++)
+    private:
+        int fitness;
+        std::string DNA;
+    public:
+        Entity()
         {
-            if(rand_num(100) < crossover_pro)
-            {
-                b[i] = a[i];
-            }
+            DNA = rand_str(goal.length());
+            fitness = calc_fitness(DNA);
         }
-    }
-    else
-    {
-        for(int i = 0; i<a.length(); i++)
+        Entity(const std::string& str) : DNA(str)
         {
-            if(rand_num(100) < crossover_pro)
+            fitness = calc_fitness(str);
+        }
+        int calc_fitness (std::string str)
+        {
+            int cnt = 0;
+            for(int i = 0; i<str.length(); i++)
             {
-                a[i] = b[i];
+                if(goal[i] == str[i]) cnt++;
+            }
+
+            return cnt;
+        }
+
+        void mutate ()
+        {
+            if(rand_num(100) < mutate_pro)
+            {
+                int id = rand_num(DNA.length());
+                DNA[id] = 'A' + rand_num('Z' - 'A');
             }
         }
 
+        std::string get_str() const {return DNA;}
+        int get_fit() const {return fitness;}
+};
+
+
+Entity crossover (const Entity& a, const Entity& b)
+{
+    std::string new_str = "";
+    std::string str_a = a.get_str();
+    std::string str_b = b.get_str();
+
+    for(int i = 0; i< str_a.length(); i++)
+    {
+        if(std::rand() % 2  == 0)
+        {
+            new_str += str_a[i];
+        }
+        else {
+            new_str += str_b[i];
+        }
     }
+
+    //std::cout << str_a << " "<< str_b << ":" << new_str << std::endl;
+    //getchar();
+    return Entity(new_str);
+}
+
+bool cmp (const Entity& a, const Entity& b)
+{
+    if(a.get_fit() > b.get_fit())
+        return true;
+
+    return false;
 }
 
 int main()
 {
-    std::vector<std::string> list;
+    srand(time(NULL));
+    std::vector<Entity> vec;
 
     for(int i = 0; i<pop_size; i++)
-    {
-        std::string str = rand_str(goal.length());
-        list.push_back(str);
-    }
+        vec.push_back(Entity());
 
+    int gen = 0;
     int best = 0;
-    while(best != goal.length())
+    while(best != goal.length() && gen < max_gen)
     {
-        for(std::vector<std::string>::iterator i = list.begin(); i != list.end(); i++)
+        gen++;
+        std::vector<Entity> new_entities;
+        for(std::vector<Entity>::iterator i = vec.begin(); i != vec.end(); i++)
         {
-            mutate(*i);
-            crossover(*i, list.at(rand_num(list.size())));
+
+            (*i).mutate();
+
+            Entity a = vec.at(rand_num(vec.size()));
+            Entity b = vec.at(rand_num(vec.size()));
+            Entity new_ent = crossover(a, b);
+
+            new_entities.push_back(new_ent);
         }
 
-        int min = goal.length();
-        int id = 0;
-        for(int i = 0; i<list.size(); i++)
+        vec.insert(vec.end(), new_entities.begin(), new_entities.end());
+
+        std::sort(vec.begin(), vec.end(), cmp);
+
+        int cnt = vec.size() - pop_size;
+        for(int i = 0; i<cnt; i++)
         {
-            if(fitness(list.at(i)) < min)
-            {
-                min = fitness(list.at(i));
-                id = i;
-            }
+            vec.erase(vec.end());
         }
 
-        list.erase(list.begin() + id);
-        std::string str = rand_str(goal.length());
-        list.push_back(str);
-
-        int max = 0;
-        id = 0;
-        for(int i = 0; i<list.size(); i++)
-        {
-            if(fitness(list.at(i)) > max)
-            {
-                max = fitness(list.at(i));
-                id = i;
-            }
-        }
-
-        std::cout << fitness(list.at(id)) << " " << list.at(id) << std::endl;
+        best = (*vec.begin()).get_fit();
+        std::cout << gen << ". generation " << best << " " << (*vec.begin()).get_str() << std::endl;
     }
 
     return 0;
